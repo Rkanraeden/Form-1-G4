43047786cadf0b400035324185eaab2f5b27c567
Rkanraeden <lecortezc@gmail.com>
2022-05-10 19:04:28 -0400

    second commit: 20220510

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
   end
 
   def create
-    User.create(email: params[:email], age: params[:age], comment: params[:comment])
+    @user = User.create(email: params[:email], age: params[:age], comment: params[:comment])
 
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_10_012913) do
+ActiveRecord::Schema.define(version: 2022_05_10_224918) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "age"
+    t.string "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
