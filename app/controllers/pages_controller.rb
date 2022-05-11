9765d0203dc617e942476666abc139e43931b83a
Rkanraeden <lecortezc@gmail.com>
2022-05-10 21:47:59 -0400

    final commit 20221005

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,11 @@ class PagesController < ApplicationController
   def index
   end
 
+  def dashboard
+    @users = User.find_valid
+
+  end
+
   def create
     @user = User.create(email: params[:email], age: params[:age], comment: params[:comment])
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :posts
   get 'pages/index'
   get 'pages/create'
+  get '/pages/dashboard'
   root 'pages#index'
 end
 
